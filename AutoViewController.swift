14e3f0a4650807a224c610bebe0c9cccd08fc217
paularsmuz <paularsmuz@gmail.com>
2017-07-15 06:55:15 +0300

    AutoViewController 1

---
+++ AutoViewController.swift
@@ -0,0 +1,40 @@
+//
+//  AutoViewController.swift
+//  Auto
+//
+//  Created by Павел Музычкин on 13.07.17.
+//  Copyright © 2017 MPA. All rights reserved.
+//
+
+import UIKit
+
+class AutoViewController: UIViewController {
+
+    @IBOutlet weak var matsSwitch: UISwitch!
+    
+    @IBOutlet weak var protectSwitch: UISwitch!
+    
+    @IBOutlet weak var winterSwith: UISwitch!
+    
+    @IBOutlet weak var diskSwitch: UISwitch!
+    
+    
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    
+    
+    @IBAction func okButton(_ sender: Any) {
+        
+    }
+    
+
+}
